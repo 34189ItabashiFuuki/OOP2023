0069e99dbcf33b3b58879173278fb35bdb7ec4df
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-15 11:02:42 +0900

    CSVファイル読み込み処理追加_P60

--- Chapter02/売上集計プログラム/SalesCalculator/Program.cs
+++ Chapter02/売上集計プログラム/SalesCalculator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,29 @@ using System.Threading.Tasks;
 namespace SalesCalculator {
     class Program {
         static void Main(string[] args) {
+            ReadSales("Sales.csv");
         }
+
+        //売上データ読み込み、Saleオブジェクトのリストを返す
+        static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();//売り上げデータを格納
+            string[] lines = File.ReadAllLines(filePath);//ファイルからすべてのデータを読み込み
+
+            foreach (string line in lines)//すべての行から一行ずつ取り出す
+            {
+                string[] items = line.Split(',');//区切りで項目別に分ける
+                Sale sale = new Sale//Saleインスタンスを生成
+                {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);//Saleインスタンスをコレクションに追加
+            }
+
+            return sales;
+
+        }
+
     }
 }
