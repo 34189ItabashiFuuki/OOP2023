2e37ea9b12a1d478dd62afc1cbcc5e2158d01edb
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-22 11:58:22 +0900

    ラムダ式以前（デリゲート、匿名メソッド）

--- Chapter02/演習問題/Exercise03/Program.cs
+++ Chapter02/演習問題/Exercise03/Program.cs
@@ -7,8 +7,10 @@ using System.Threading.Tasks;
 namespace Exercise03 {
     class Program {
         static void Main(string[] args) {
+            Console.WriteLine("＊＊売上集計＊＊");
             Console.WriteLine("１：店舗別売上");
             Console.WriteLine("２：商品カテゴリー別売上");
+            Console.Write(">");
             int count = int.Parse(Console.ReadLine());
             var sales = new SalesCounter(@"data\sales.csv");
             if (count == 1)
