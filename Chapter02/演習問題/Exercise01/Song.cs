27d77d29356840f8ffdb4437f8e01d1c431b47f9
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-22 09:35:29 +0900

    問題2.3作成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    //2.1.1
+    class Song {
+        //歌のタイトル
+        public string Title { get; set; }
+        //アーティスト名
+        public string ArtistName { get; set; }
+        //演奏時間、単位は秒
+        public int Length { get; set; }
+        
+        //2.1.2
+        //コンストラクタ
+        public Song(string title, string artistname, int length) {
+            Title = title;
+            ArtistName = artistname;
+            Length = length;
+        }
+
+        
+    }
+}
