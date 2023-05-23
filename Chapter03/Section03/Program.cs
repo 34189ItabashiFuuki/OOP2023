d06ee127fa2a315935b98a49683e0c8518f584bd
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-23 11:13:55 +0900

    p90

---
+++ Chapter03/Section03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section03 {
+    class Program {
+        static void Main(string[] args) {
+            var list = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+            //var exists = list.Exists(s => s[0] == 'A');
+            //Console.WriteLine(exists);
+
+            //var index = list.FindIndex(s => s == "Canberra");
+            //Console.WriteLine(index);
+
+            var names = list.ConvertAll(s => s.ToLower());
+            names.ForEach(s => Console.WriteLine(s));
+        }   
+    }
+}
