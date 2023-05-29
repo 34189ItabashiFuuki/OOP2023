71f2e93b2ada856ed6d3b3d2da80e60b38e497d2
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-29 12:09:44 +0900

    TryParse作成

--- Chapter04/Section04/Program.cs
+++ Chapter04/Section04/Program.cs
@@ -9,26 +9,45 @@ namespace Section04 {
         static void Main(string[] args) {
 
             #region 条件演算子            
-            var list = new List<int> { 10, 20, 30, 40, };
+            /*var list = new List<int> { 10, 20, 30, 40, };
             var key = 40;
 
             var num = list.Contains(key) ? 1 : 0;//条件演算子・三項演算子
-            Console.WriteLine(num);
+            Console.WriteLine(num);*/
             #endregion
 
             #region null合体演算子
-            string code = "12345";
+            /*string code = "12345";
             var message = GetMessage(code) ?? DefaultMessege();
-            Console.WriteLine(message);
+            Console.WriteLine(message);*/
+            #endregion
+
+            #region null条件演算子
+            Sale sale = new Sale {
+                Amount = 100,
+            };
+            //「int」はnull許容型、「?.」はnull条件演算子
+            int? ret = sale?.Amount;
+            Console.WriteLine(ret);
             #endregion
         }
-
         private static object GetMessage(object code) {
             return 123;
         }
-
         private static object DefaultMessege() {
             return "Default Message";
         }
     }
+    //売上クラス
+    public class Sale {
+        //店舗名
+        public string ShopName { get; set; }
+        //商品カテゴリー
+        public string ProductCategory { get; set; }
+        //売上高
+        public int Amount { get; set; }
+    }
 }
+
+
+
