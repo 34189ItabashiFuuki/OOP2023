89a9f685ec75c6037bc86399e23aaf813288a14c
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-29 11:07:09 +0900

    null合体演算子

---
+++ Chapter04/Section04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section04 {
+    class Program {
+        static void Main(string[] args) {
+
+            #region 条件演算子            
+            var list = new List<int> { 10, 20, 30, 40, };
+            var key = 40;
+
+            var num = list.Contains(key) ? 1 : 0;//条件演算子・三項演算子
+            Console.WriteLine(num);
+            #endregion
+
+            #region null合体演算子
+            string code = "12345";
+            var message = GetMessage(code) ?? DefaultMessege();
+            Console.WriteLine(message);
+            #endregion
+        }
+
+        private static object GetMessage(object code) {
+            return 123;
+        }
+
+        private static object DefaultMessege() {
+            return "Default Message";
+        }
+    }
+}
