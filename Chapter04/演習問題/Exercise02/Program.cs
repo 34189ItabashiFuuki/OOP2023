4a127e2bed100d5063d32da8a3eb370c4c309dbb
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-30 12:11:28 +0900

    4.1完成

---
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exercise01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            var ym = new YearMonth(2023, 11);
+            var c21 = ym.Is21Century;
+            var ymNestMonth = ym.AddOneMonth();
+
+            Console.WriteLine(ym); //○○○○年　△月
+            Console.WriteLine(ymNestMonth);
+        }
+    }
+}
