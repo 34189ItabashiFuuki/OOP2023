09b899907849320ca148c1d714cb1947c0b9171b
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-05-31 11:42:40 +0900

    4.2完成

--- Chapter04/演習問題/Exercise02/Program.cs
+++ Chapter04/演習問題/Exercise02/Program.cs
@@ -9,12 +9,58 @@ namespace Exercise02 {
     class Program {
         static void Main(string[] args) {
 
-            var ym = new YearMonth(2023, 11);
-            var c21 = ym.Is21Century;
-            var ymNestMonth = ym.AddOneMonth();
+            // 4.2.1
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980, 1),
+                new YearMonth(1990, 4),
+                new YearMonth(2000, 7),
+                new YearMonth(2010, 9),
+                new YearMonth(2020, 12),
+            };
 
-            Console.WriteLine(ym); //○○○○年　△月
-            Console.WriteLine(ymNestMonth);
+            // 4.2.2
+            Console.WriteLine("\n- 4.2.2 ---");
+            Exercise2_2(ymCollection);
+            Console.WriteLine("\n- 4.2.4 ---");
+
+            // 4.2.4
+            Exercise2_4(ymCollection);
+            Console.WriteLine("\n- 4.2.5 ---");
+
+
+            // 4.2.5
+            Exercise2_5(ymCollection);
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach(var item in ymCollection) {
+                Console.WriteLine(item);
+            }
+        }
+
+        //4.2.3
+        static YearMonth FindFirst21C(YearMonth[] yms) {
+            foreach (var item in yms){
+                if(item.Is21Century) {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            if (FindFirst21C(ymCollection) == null) {
+                Console.WriteLine("２１世紀のデータはありません。");
+            }
+            else {
+                Console.WriteLine(FindFirst21C(ymCollection).Year);
+            }
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var array = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+
+            Exercise2_2(array);
         }
     }
 }
