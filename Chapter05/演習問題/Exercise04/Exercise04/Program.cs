7eb55adf782fc1c309891b54753f7050ec3e88ba
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-06-07 11:58:22 +0900

    6.1の3まで

--- Chapter05/演習問題/Exercise04/Exercise04/Program.cs
+++ Chapter05/演習問題/Exercise04/Exercise04/Program.cs
@@ -11,8 +11,6 @@ namespace Exercise04 {
         static void Main(string[] args) {
             Stopwatch sw = new Stopwatch();
             sw.Start();
-
-
 #if NonArray
             var line = "Novelist=谷崎潤一郎;BestWork=春琴抄;Born=1886";
             string[] bookinfo = line.Split(';','=').ToArray();
@@ -26,25 +24,17 @@ namespace Exercise04 {
                 "Novelist=太宰治;BestWork=人間失格;Born=1909",
                 "Novelist=宮沢賢治;BestWork=銀河鉄道の夜;Born=1896",
             };
-            foreach (var line in lines)
-            {
-                foreach (var pair in line.Split(';'))
-                {
+            foreach (var line in lines) {
+                foreach (var pair in line.Split(';')) {
                     var array = pair.Split('=');
                     Console.WriteLine("{0} : {1}", ToJapanese(array[0]),array[1]);
-                    
                 }
             }
-
-
-
 #endif
             Console.WriteLine("実行時間 = {0}",sw.Elapsed.ToString(@"ss\.fffff"));//時間表示
         }
-
         private static string ToJapanese(string key) {
-            switch (key)
-            {
+            switch (key)　{
                 case "Novelist":
                     return "作家　";
                 case "BestWork":
