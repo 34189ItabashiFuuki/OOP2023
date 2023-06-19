52f61bcc3cfef5fc9e03c74428305acac7e88a38
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-06-19 14:00:18 +0900

    問題6.1完成

--- Chapter06/演習問題/Exercise01/Program.cs
+++ Chapter06/演習問題/Exercise01/Program.cs
@@ -32,31 +32,27 @@ namespace Exercise01 {
 
         private static void Exercise1_2(int[] numbers) {
             var lastnum = numbers.Length - 2;
-
             foreach (var nums in numbers.Skip(lastnum))
-            {
                 Console.WriteLine(nums);
-            }
+            
         }
 
         private static void Exercise1_3(int[] numbers) {
-
             var nums = numbers.Select(x => x.ToString());
             foreach (var n in nums)
-            {
                 Console.WriteLine(n);
-            }
         }
 
         private static void Exercise1_4(int[] numbers) {
-
-
-
-
-
+            foreach (var n in numbers.OrderBy(n => n).Take(3))
+                Console.WriteLine(n);
         }
 
         private static void Exercise1_5(int[] numbers) {
+            var results = numbers.Distinct();
+
+            Console.WriteLine(results.Where(n => n > 10).Count());           
+
         }
     }
 }
