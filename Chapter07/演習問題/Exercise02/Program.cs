32236df7174d72df884c5609692f55c7c51b259d
板橋風輝 <OIBC34189@ojs.ac.jp>
2023-06-30 10:21:26 +0900

    問題7.2完成

---
+++ Chapter07/演習問題/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            //コンストラクタの呼び出し
+            var abbrs = new Abbreviations();
+
+            //Addメソッドの呼び出し
+            abbrs.Add("IOC", "国際オリンピック委員会");
+            abbrs.Add("NTP", "核兵器不拡散条約");
+
+            //7.2.3
+            //上のAddメソッドで。２つのオブジェクトを追加しているので、読み込み
+
+            int count = abbrs.Count;
+            Console.WriteLine(count);
+
+            //7.2.3(Removeの呼び出し)
+            Console.WriteLine("けすやつ");
+            var remove = Console.ReadLine();
+            abbrs.Remove(remove);
+
+
+            //7.2.4
+            //IEnumerabl<>を実装したので、LINQが使える。
+            abbrs.abbs();
+
+
+        }
+    }
+}
