07a67cbcd0315b1aa1bb913000ca488f757368b4
板橋　風輝 <oibc34189@ojs.ac.jp>
2023-10-19 12:04:08 +0900

    問題17.2完成

---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/KilometerConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    class KilometerConverter : Framework.ConverterBase {
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "Kilometer" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1000; } }
+        public override string UnitName { get { return "キロメートル"; } }
+    }
+}
---
+++ Chapter17/演習問題/Exercise02/ConcreteConverter/mileConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02.ConcreteConverter {
+    class mileConverter : Framework.ConverterBase {
+
+        public override bool IsMyUnit(string name) {
+            return name.ToLower() == "mile" || name == UnitName;
+        }
+        protected override double Ratio { get { return 1609.433; } }
+        public override string UnitName { get { return "マイル"; } }
+
+    }
+}
